b43296d7a45f2dc1472de4d20d61421e2fe34568
nsxsoft <nsxsoft@gmail.com>
2020-04-23 06:43:22 -0400

    add terre route

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/mars', function(req, res) {
   res.send('Hello Mars!\n');
 });
 
+app.get('/terre', function(req, res) {
+  res.send('Hello Terre!\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
